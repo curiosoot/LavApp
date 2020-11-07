ce801b20844241ec4a2f399ccfd314e05eb2ba72
Duberfernandezz <Duberfernandezz@gmail.com>
2020-11-06 23:29:58 -0500

    CRUD usuario_rol

---
+++ Proyecto_LavApp/Proyecto_LavApp/Controllers/usuario_rol_Controller.cs
@@ -0,0 +1,116 @@
+﻿using Proyecto_LavApp.Datos;
+using Proyecto_LavApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_LavApp.Controllers
+{
+    public class usuario_rol_Controller : Controller
+    {
+        List<SelectListItem> listroles;
+        List<SelectListItem> listusuarios;
+
+        usuario_rol_Admin admin = new usuario_rol_Admin();
+        // GET: usuario_rol_
+        public ActionResult Index()
+        {
+            return View(admin.Consultar());
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            return View(admin.Consultar(id));
+        }
+
+        public ActionResult Crear()
+        {
+            llenar_roles();
+            ViewBag.listaroles = listroles;
+
+            llenar_usuarios();
+            ViewBag.listausuarios = listusuarios;
+
+            return View();
+        }
+        public ActionResult Guardar(usuario_rol modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                llenar_roles();
+                ViewBag.listaroles = listroles;
+
+                llenar_usuarios();
+                ViewBag.listausuarios = listusuarios;
+
+                return View(modelo);
+            }
+            else
+            {
+                admin.Guardar(modelo);
+                //return View("Crear", modelo);
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            llenar_roles();
+            ViewBag.listaroles = listroles;
+
+            llenar_usuarios();
+            ViewBag.listausuarios = listusuarios;
+            return View(admin.Consultar(id));
+        }
+
+        public ActionResult Modificar(usuario_rol modelo)
+        {
+            admin.Modificar(modelo);
+            llenar_roles();
+            ViewBag.listaroles = listroles;
+
+            llenar_usuarios();
+            ViewBag.listausuarios = listusuarios;
+            return View("Editar", modelo);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            usuario_rol modelo = admin.Consultar(id);
+            admin.Eliminar(modelo);
+            return View("Index", admin.Consultar());
+        }
+
+        private void llenar_roles()
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                listroles = (from roles in contexto.roles
+                                 //where tipo_documento.sn_activo == true
+                                 select new SelectListItem
+                                 {
+                                     Text = roles.txt_desc_rol,
+                                     Value = roles.id_rol.ToString()
+                                 }).ToList();
+                listroles.Insert(0, new SelectListItem { Text = "Seleccione", Value = "" });
+            }
+        }
+
+        private void llenar_usuarios()
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                listusuarios = (from usuarios in contexto.usuarios
+                                 //where tipo_documento.sn_activo == true 
+                                 select new SelectListItem
+                                 {
+                                     Text = usuarios.Username,
+                                     Value = usuarios.id_usuario.ToString()
+                                 }).ToList();
+                listusuarios.Insert(0, new SelectListItem { Text = "Seleccione", Value = "" });
+            }
+        }
+    }
+}
---
+++ Proyecto_LavApp/Proyecto_LavApp/Datos/usuario_rol_Admin.cs
@@ -0,0 +1,55 @@
+﻿using Proyecto_LavApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_LavApp.Datos
+{
+    public class usuario_rol_Admin
+    {
+        public IEnumerable<usuario_rol> Consultar()
+        {
+            //var persona = new personas();
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                return contexto.usuario_rol.Include(x => x.roles).ToList();
+            }
+        }
+        public usuario_rol Consultar(int id)
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                return contexto.usuario_rol.Include(x => x.usuarios).FirstOrDefault(c => c.id_usuario_rol == id);
+            }
+        }
+
+        public void Guardar(usuario_rol modelo)
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                contexto.usuario_rol.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Modificar(usuario_rol modelo)
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                contexto.Entry(modelo).State = EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Eliminar(usuario_rol modelo)
+        {
+            using (LavApp_BDEntities contexto = new LavApp_BDEntities())
+            {
+                contexto.Entry(modelo).State = EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
